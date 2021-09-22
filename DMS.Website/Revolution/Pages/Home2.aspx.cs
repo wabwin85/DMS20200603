802d31e225d093887502e6c832956f6fdd503e4c
wabwin85 <aibing.wang@gmedtech.com>
2021-09-22 15:30:41 +0800

    check in

--- DMS.Website/Revolution/Pages/Home2.aspx.cs
+++ DMS.Website/Revolution/Pages/Home2.aspx.cs
@@ -11,7 +11,7 @@ namespace DMS.Website.Revolution.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string a = "wang";
+            
         }
     }
 }
